471e8ad5560b70395d62cd56981c25d9440cb175
Brandon <brandon@kansas.net>
2020-08-31 22:47:14 -0500

    typo

--- wwwroot/inc/breed-ngos.php
+++ wwwroot/inc/breed-ngos.php
@@ -4,13 +4,7 @@
 # framework. See accompanying file "COPYING" for the full copyright and
 # licensing information.
 
-<?php
 
-# This file is a part of RackTables, a datacenter and server room management
-# framework. See accompanying file "COPYING" for the full copyright and
-# licensing information.
-
-// Functions for HP Procurve switches
 
 function ngosReadLLDPStatus ($input)
 {
